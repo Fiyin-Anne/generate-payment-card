18001359375e62048e68905102cc0c0ba4238803
fiyin <fiyinakinsiku.b@gmail.com>
2021-04-22 13:56:08 +0100

    update tests

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Generate payment card
-This package generates a valid payment card number.
+This package generates a valid payment card number. A payment card can either be a credit, debit or otherwise.
 
 [![npm version](https://badge.fury.io/js/generate-payment-card.svg)](https://badge.fury.io/js/generate-payment-card) [![Coverage Status](https://coveralls.io/repos/github/Fiyin-Anne/generate-payment-card/badge.svg)](https://coveralls.io/github/Fiyin-Anne/generate-payment-card) [![Node.js CI](https://github.com/Fiyin-Anne/generate-payment-card/actions/workflows/node.js.yml/badge.svg)](https://github.com/Fiyin-Anne/generate-payment-card/actions/workflows/node.js.yml)
 
 
 ### 🚀 Getting started
-Install the package from npm using `npm install generate-payment-card`.
+Install the package from npm.
+
+`npm install generate-payment-card`.
 
 ### 🧰 Basic usage
 ```javascript
-var { generate } = require('generate-payment-card')
-var payment_card_details = generate({
+var generatePaymentCard = require('generate-payment-card')
+var payment_card_details = generatePaymentCard.generate({
     "card_brand": "american express",
     "user_digits": {
         "status": true,
@@ -29,6 +31,8 @@ console.log(payment_card_details)
 */
 ```
 
+### Request payload
+
 The `generate` function takes in one argument which is an object containing a card_brand (string), user_digits (object) and `issuer` (string).
 - `card_brand`: This is the card brand that the user would like to generate a payment card from. Currently, this package is only able to generate Mastercard, Visa, American Express and Discover cards.
 - `user_digits`: The user can specify a sequence of not more or less than ten digits which they want to be a part of the payment card number. They can also specify where they want this sequence to be placed by setting `position` to `startswith`, `contains` or `endswith`. This object accepts three keys:
@@ -36,3 +40,14 @@ The `generate` function takes in one argument which is an object containing a ca
     - `digits`: This is the sequence of 10 digits that the user provides.
     - `position`: This indicates where the user wants the digits to be placed in the the generated card numbers.
 - `issuer`: This is the specific organization that the user wants the card to be generated from.
+
+### Response payload
+
+Once a payment card number has been generated successfully, it reyurn an object consisting of the following:
+
+- `valid_card_number`: This is the valid card number created.
+- `cvv`: This is the card verification value usually found at the back of a payment card. Here, it is generated randomly.
+- `expiry_date`: The user can pass their prefered length of years between 1 and 5 after which the card should expire. The default value is 5.
+- `balance`: The user can also pass their prefered balance amount between ten thousand and five million. The default value is 0.
+- `issuer`: This is the specific organization that the card was generated from.
+- `card_brand`: This refers to the brand of the payment card, either `Visa`, `Mastercard`, `Discover`, `American Express` for now.
--- test/test.test.js
+++ test/test.test.js
@@ -23,6 +23,32 @@ describe('GENERATE PAYMENT CARD', function () {
 		expect(card_number_details.card_brand).to.equal('american express');
 		expect(card_number_details).to.have.property('valid_card_number');
 		expect(card_number_details).to.have.property('issuer');
+		expect(card_number_details).to.have.property('cvv');
+		expect(card_number_details).to.have.property('balance');
+		expect(card_number_details).to.have.property('expiry_date');
+	});
+
+	it('<with specified expiry date> Should generate a valid payment card number', function (){
+
+		var test_params = {
+            "card_brand": "american express",
+            "user_digits": {
+                "status": true,
+                "digits": 2345678991,
+                "position": "endswith"
+            },
+			"expires_in": 3
+		};
+
+        var card_number_details = generateCardNumber(test_params);
+        var validation = validateLuhn(card_number_details.valid_card_number);
+
+		var verify_year = new Date(card_number_details.expiry_date).getFullYear() - new Date(Date.now()).getFullYear();
+
+		expect(validation).to.equal(`${card_number_details.valid_card_number} is a valid payment card number`);
+		expect(verify_year).to.equal(test_params.expires_in);
+		expect(card_number_details).to.have.property('valid_card_number');
+		expect(card_number_details).to.have.property('issuer');
 		expect(card_number_details).to.have.property('card_brand');
 	});
 
@@ -48,7 +74,7 @@ describe('GENERATE PAYMENT CARD', function () {
 	});
 
 
-	it('<no card brand specified> Should generate a valid payment card number', function (){
+	it('<no card brand specified> Should generate a valid visa payment card number', function (){
 
 		var test_params = {
             "user_digits": {
@@ -133,6 +159,26 @@ describe('GENERATE PAYMENT CARD', function () {
 		expect(card_number_details).to.have.property('card_brand');
 	});
 
+	it('<balance specified> Should generate a valid payment card number', function (){
+
+		var test_params = {
+			"card_brand": "visa",
+            "user_digits": {
+                "status": true,
+                "digits": 3745678991,
+                "position": "contains"
+            },
+			"issuer": "bank of america",
+			"balance": 200000,
+		};
+
+        var card_number_details = generateCardNumber(test_params);
+        var validation = validateLuhn(card_number_details.valid_card_number)
+
+		expect(validation).to.equal(`${card_number_details.valid_card_number} is a valid payment card number`);
+		expect(card_number_details.balance).to.equal(test_params.balance);
+	});
+
 	it('<card brand not available for specific issuer> Should return error', function (){
 
 		var test_params = {
@@ -148,5 +194,74 @@ describe('GENERATE PAYMENT CARD', function () {
 		expect(() => { generateCardNumber(test_params); }).to.throw("This card brand is not available for this company or company does not exist in our database.");
 	
 	});
+
+	it('<with specified expiry date greaterr than 5> Should throw error', function (){
+
+		var test_params = {
+            "card_brand": "american express",
+            "user_digits": {
+                "status": true,
+                "digits": 2345678991,
+                "position": "endswith"
+            },
+			"expires_in": 10
+		};
+
+		expect(() => { generateCardNumber(test_params); }).to.throw("Expiry year must be between 1 and 5.");
+	
+	});
+
+	it('<balance specified greater than 5million> Should throw error', function (){
+
+		var test_params = {
+			"card_brand": "visa",
+            "user_digits": {
+                "status": true,
+                "digits": 3745678991,
+                "position": "contains"
+            },
+			"issuer": "bank of america",
+			"balance": 200000000,
+		};
+
+        expect(() => { generateCardNumber(test_params); }).to.throw("Balance must be between 10000 and 5000000.");
+	
+	});
+
+	it('<balance specified greater than 5million> Should throw error', function (){
+
+		var test_params = {
+			"card_brand": "visa",
+            "user_digits": {
+                "status": true,
+                "digits": 3745678991,
+                "position": "contains"
+            },
+			"issuer": "bank of america",
+			"balance": 2000,
+		};
+
+        expect(() => { generateCardNumber(test_params); }).to.throw("Balance must be between 10000 and 5000000.");
+	
+	});
+
+	it('<card brand not supported> Should throw error', function (){
+
+		var test_params = {
+			"card_brand": "Maestro",
+            "user_digits": {
+                "status": true,
+                "digits": 3745678991,
+                "position": "contains"
+            },
+			"issuer": "bank of america",
+			"balance": 2000,
+		};
+
+        expect(() => { generateCardNumber(test_params); }).to.throw("card brand is not currently supported. [available: 'visa', 'mastercard', 'discover', 'american express'].")
+	
+	});
+	
+
 
 })
